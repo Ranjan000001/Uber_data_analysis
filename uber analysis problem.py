f406b58e8a671da20280ba452050ab788f04bf5c
Ranjan000001 <ranjan.chau14@gmail.com>
2025-05-12 04:45:15 +0530

    Update uber analysis problem.py
    update code

--- uber analysis problem.py
+++ uber analysis problem.py
@@ -1,13 +1,17 @@
-# import laibraries
+# import libraries
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+
 # Read files
 dataset = pd.read_csv("C:\\Users\\user\\Downloads\\UberDataset.csv")
 # information of data
 table = dataset.info
 
 Head = dataset.head()
+# check null cells
+print(data.isnull().sum())
 
 # Data cleaning
 dataset["PURPOSE"].fillna("Not")
@@ -32,10 +36,16 @@ plt.figure(figsize=(20, 5))
 
 plt.subplot(1, 2, 1)
 Figure1 = sns.countplot(x=dataset["CATEGORY"])
+plt.show()
+
 # use sns count plot function for draw the graph on the bases of category
 figure2 = sns.countplot(x=dataset["PURPOSE"])
+plt.show()
+
 # sns count plot function is counting the no. of common values in column and put in the graph
 figure3 = sns.countplot(x=dataset["period"])
+plt.show()
+
 # graph on period column
 dataset["month"] = pd.DatetimeIndex(dataset["date"]).month
 # month column and its elements i 1,2,3,4 ect.
@@ -45,9 +55,11 @@ dataset["month"] = dataset.month.map(month_d)
 # map function for containing column month with dictionary month_d
 month_count = dataset.month.value_counts(sort=False)
 # it counts the total no. jan, feb, mar upto dec.
-df = pd.DataFrame({'Month': month_count.values, 'count value': dataset.groupby('month', sort=False)["MILES"].max()})
+data = {'Month': month_count.values, 'count value': dataset.groupby('month', sort=False)["MILES"].max()}
+df = pd.DataFrame(data).reset_index()
 # data frame where month store total no. of times month repeated and count value store the maximum miles of the month
-sns.lineplot(data=df,  x='Month', y='count value')
+sns.lineplot(data=df,  x='month', y='count value')
+plt.show()
 dataset["day"] = dataset.START_DATE.dt.weekday
 # column contain numbers such 0,1,2,3,4,5 and 6
 day_dis = {0: 'Mon', 1: 'Tue', 2: 'Wed', 3: 'Thu', 4: 'Fri', 5: 'Sat', 6: 'Sun'}
@@ -57,9 +69,20 @@ dataset["day"] = dataset["day"].map(day_dis)
 day_count = dataset.day.value_counts(sort=False)
 # count the total no. of time days repeated
 sns.barplot(x=day_count.index, y=day_count)
+plt.show()
 
 sns.boxplot(dataset["MILES"])
+plt.show()
+
 sns.boxplot(dataset[dataset["MILES"] < 100]["MILES"])
+plt.show()
+
 sns.boxplot(dataset[dataset["MILES"] < 40]["MILES"])
-sns.distplot(dataset[dataset["MILES"] < 40]["MILES"])
-print(dataset.info())
+plt.show()
+
+sns.boxplot(dataset[dataset["MILES"] < 30]["MILES"])
+plt.show()
+
+sns.displot(dataset[dataset["MILES"] < 30]["MILES"])
+plt.show()
+
